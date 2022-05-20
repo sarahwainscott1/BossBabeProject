8584f39a8ff7e463d05c48b2e51ab12ead1e09c9
Sarah Wainscott <swainscott@tql.com>
2022-05-20 15:20:59 -0400

    work controller

--- BossBabeProject/WorkController.cs
+++ BossBabeProject/WorkController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Data.SqlClient;
+﻿using BossBabeProjectLibrary.Models;
+using Microsoft.Data.SqlClient;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,21 +9,42 @@ using System.Threading.Tasks;
 namespace BossBabeProjectLibrary {
     public class WorkController {
 
-        public string ConnectionString { get; set; } = null!;
-        public SqlConnection SqlConnection { get; set; } = null!;
+        public static void GetAllWork() {
+            var _context = new AppDbContext();
+            var works = _context.Works.ToList();
+            foreach (var w in works) {
+                Console.WriteLine(w);
 
-        public WorkController(string ServerInstance, string Database) {
-            ConnectionString = $"server ={ServerInstance};" + $"database ={Database};" + "trustservercertificate=true;" + "trusted_connection = true;";
-        }
-        public void OpenConnection() {
-            SqlConnection = new SqlConnection(ConnectionString);
-            SqlConnection.Open();
-            if (SqlConnection.State != System.Data.ConnectionState.Open) {
-                throw new Exception("Connection did not open :(");
             }
         }
-        public void CloseConnection() {
-            SqlConnection.Close();
+        public static void GetWorkByResource(int resourceId) {
+            var _context = new AppDbContext();
+            var works = from w in _context.Works
+                        join r in _context.Resources
+                        on w.ResourceId equals r.Id
+                        where r.Id == resourceId
+                        select new { ResourceId = r.Id, Name = r.Name, WorkId = w.Id, Hours = w.Hours, Description = w.Description };
+            works.ToList().ForEach(w => { global::System.Console.WriteLine($"Resource Id: {w.ResourceId} | Name:  {w.Name} | Work ID: {w.WorkId} | Hours: {w.Hours} | Description: {w.Description}"); });
+
         }
+        public static void AddWork(int resourceId, string description, int hours) {
+            var _context = new AppDbContext();
+            var newWork = new Work();
+            newWork.ResourceId = resourceId;
+            newWork.Description = description;
+            newWork.Hours = hours;
+            _context.Works.Add(newWork);
+            _context.SaveChanges();
+        }
+
+        public static void RemoveWork(int workId) {
+            var _context = new AppDbContext();
+            var removeWork = _context.Works.Find(workId);
+            if (removeWork != null) {
+                _context.Works.Remove(removeWork);
+            }
+        }
+    }
+}
     }
 }
--- Project/Program.cs
+++ Project/Program.cs
@@ -1,13 +1,5 @@
 ﻿using BossBabeProjectLibrary;
 
-const string server = "localhost\\sqlexpress";
-const string database = "BossBabeProject";
 
-var projectctrlr = new ProjectController(server, database);
-projectctrlr.OpenConnection();
-
-Console.WriteLine($"{ProjectController.UpdateHoursWorked(2)}"); ;
-
-projectctrlr.CloseConnection();
 //var workctrlr = new WorkController(server, database);
 //workctrlr.OpenConnection();
